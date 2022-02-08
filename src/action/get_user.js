5c47b49be9897a88f2ea5fa86536e0048dd44222
leonid <forestmarket@yandex.ru>
2022-02-09 00:11:40 +0300

    New changelist

--- src/action/get_user.js
+++ src/action/get_user.js
@@ -1,4 +1,7 @@
 import request from "./request";
+import {render} from "react-dom";
+import close_screen from "./close_screen";
+import Profile from "../components/profile";
 
 async function get_user(user_data) {
 
@@ -7,8 +10,14 @@ async function get_user(user_data) {
     let response=await request(url, body, 'POST', null)
     let result=await response
     localStorage.token=result.data.token
-    url=''
-    let concerts='tickets.сделай.site/user/booking'
-
+    url='http://tickets.сделай.site/user'
+    response=await request(url, null, 'GET', null)
+    let user=await response
+    url='http://tickets.сделай.site/user/booking'
+    response=await request(url, null, 'GET', null)
+    let concerts=await response
+    close_screen()
+    render([<Profile user={user} concerts={concerts}/>], document.getElementsByClassName('screen')[7])
+    document.getElementsByClassName('screen')[7].style.display='flex'
 }
 export default get_user
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import Concert_data from "./concert_data";
 
-const Profile = () => {
+
+const Profile = (props) => {
+    let i=0
+    let concerts=props.concerts.data.items
+    let data_concerts=concerts.map((value)=>{
+        let date= new Date(value.tickets.date_concert)
+        if (date<new Date()) {i++}
+        else return <Concert_data data={value.tickets}/>
+    })
+
     return (
         <div>
             <section>
@@ -9,17 +19,17 @@ const Profile = () => {
                     <p className="color_red">Данные регистрации</p>
                     <div className="row_align">
                         <p className="color_blue row_item">Имя:</p>
-                        <p className="test-3-name color_white row_item"></p>
+                        <p className="test-3-name color_white row_item">{props.user.first_name}</p>
                         <p className="color_blue row_item">Фамилия:</p>
-                        <p className="test-3-last color_white row_item"></p>
+                        <p className="test-3-last color_white row_item">{props.user.last_name}</p>
                         <p className="color_blue row_item">Количество посещенных концертов:</p>
-                        <p className="test-3-num color_white row_item"></p>
+                        <p className="test-3-num color_white row_item">{i}</p>
 
                     </div>
                     <input type="button" className="test-3-logout button" value="Выйти" onClick="exit()"/>
                         <p className="color_red">Предстаящие концерты</p>
                         <div className="row_align" id="concert">
-
+                            {data_concerts}
                         </div>
                 </div>
             </section>
